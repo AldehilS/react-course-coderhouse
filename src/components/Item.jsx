41e2f63139270c8b565436d705fbb3126fe42893
Aldehil Sánchez <sanheraldehil@outlook.com>
2024-08-30 14:04:00 -0600

    Fix cards alignment and height, and Add links to item

---
+++ src/components/Item.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+export default function Item({ product, baseURL }) {
+  return (
+    <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-3 p-4">
+      <Link to={`/item/${product.id}`}>
+        <div className="card h-100">
+          <img
+            src={`${baseURL}/products/${product.image}`}
+            className="card-img-top h-75 object-fit-contain"
+            alt={product.name}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{product.name}</h5>
+            <p className="card-text">{product.description}</p>
+            <p className="card-text">
+              <small className="text-muted">${product.price}</small>
+            </p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,46 +1,42 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Item from "./Item";
 
 export default function ItemListContainer({ greeting }) {
-  const {id} = useParams();
+  const { id } = useParams();
   const [products, setProducts] = useState([]);
   const baseURL = import.meta.env.BASE_URL;
 
   useEffect(() => {
     fetch(`${baseURL}products.json`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setProducts(data);
-    })
-    .catch((error) => {
-      console.error("Error fetching products:", error);
-    });
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
   }, [id]);
 
   return (
     <>
-      <div className="container mt-5 mb-4">
+      <div className="container-fluid mt-5 mb-4 px-5 d-flex flex-column align-items-center">
         <h1 className="h1">{greeting}</h1>
-        {id ? <h2 className="h2">Category: {id}</h2> : <h2 className="h2">Showing all products</h2>}
-        <div className="row">
-          {products.map((product) => {
-            return (
-              <div key={product.id} className="col-12 col-md-6 col-lg-4 col-xl-3">
-                <div className="card mb-3">
-                  <img src={`${baseURL}/products/${product.image}`} className="card-img-top" alt={product.name} />
-                  <div className="card-body">
-                    <h5 className="card-title">{product.name}</h5>
-                    <p className="card-text">{product.description}</p>
-                    <p className="card-text">
-                      <small className="text-muted">${product.price}</small>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+        {id ? (
+          <h2 className="h2">Category: {id}</h2>
+        ) : (
+          <h2 className="h2">Showing all products</h2>
+        )}
+        <div className="container-fluid row mt-5">
+          {products.map((product) => (
+            <Item
+              key={`product${product.id}`}
+              product={product}
+              baseURL={baseURL}
+            />
+          ))}
         </div>
       </div>
     </>
