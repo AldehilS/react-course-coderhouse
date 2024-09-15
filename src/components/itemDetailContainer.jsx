49afa6cdc988b1db90400324e7250dfc324b13ba
Aldehil Sánchez <sanheraldehil@outlook.com>
2024-09-14 22:40:34 -0600

    refactor: Update import path for ItemQuantitySelector component

--- src/components/itemDetailContainer.jsx
+++ src/components/itemDetailContainer.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import ItemQuantitySelector from "./ProductCounter";
+import ItemQuantitySelector from "./ItemQuantitySelector";
 
 export default function ItemDetailContainer() {
   const { id } = useParams();
