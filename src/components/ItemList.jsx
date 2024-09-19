e1deac2906e30a19d4321e17f68e1d89e9d582b3
Aldehil Sánchez <sanheraldehil@outlook.com>
2024-09-19 14:00:32 -0600

    Implemented ItemList Component

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,15 @@
+import Item from "./Item";
+
+export default function ItemList({ products, baseURL }) {
+  return (
+    <div className="container-fluid row mt-5">
+      {products.map((product) => (
+        <Item
+          key={`product${product.id}`}
+          product={product}
+          baseURL={baseURL}
+        />
+      ))}
+    </div>
+  );
+}
