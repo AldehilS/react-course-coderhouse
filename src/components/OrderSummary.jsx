c5a64af1a82d6e523641a468f58ea1c960777361
Aldehil Sánchez <sanheraldehil@outlook.com>
2024-09-23 16:18:43 -0600

    Add OrderSummary component

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function OrderSummary({ subtotal }) {
   const taxPercentage = 0.16;
 
@@ -8,10 +10,18 @@ export default function OrderSummary({ subtotal }) {
         <hr />
         <p className="text-start">Subtotal: ${subtotal.toFixed(2)}USD</p>
         <p className="text-start">Shipping: Free</p>
-        <p className="text-start">Taxes ({taxPercentage*100}%): ${(subtotal*taxPercentage).toFixed(2)}USD</p>
+        <p className="text-start">
+          Taxes ({taxPercentage * 100}%): $
+          {(subtotal * taxPercentage).toFixed(2)}USD
+        </p>
         <hr />
-        <p className="text-start">Total: ${(subtotal + (subtotal * taxPercentage)).toFixed(2)}USD</p>
+        <p className="text-start">
+          Total: ${(subtotal + subtotal * taxPercentage).toFixed(2)}USD
+        </p>
+        <Link to="/checkout" className="btn btn-warning">
+          Proceed to checkout
+        </Link>
       </div>
     </>
-  )
+  );
 }
