ebf7fa50dcfa56947ee66dde43b5d0fa6185486f
Aldehil Sánchez <sanheraldehil@outlook.com>
2024-09-24 15:44:06 -0600

    Refactor CartItem component to fix delete functionality

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -8,7 +8,7 @@ export default function CartItem({ product, quantity, onDelete }) {
     <>
       <button
         className="btn btn-secondary position-absolute top-0 end-0 p-1 mt-2 me-2"
-        onClick={() => onDelete(id)}
+        onClick={() => onDelete(product.id)}
       >
         <MdDelete className="delete-icon" />
       </button>
